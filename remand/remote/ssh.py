b821da5dcfcdd6b979c6bc5904e2946a3483a364
Marc Brinkmann <git@marcbrinkmann.de>
2015-01-08 14:26:57 +0100

    Updated known_hosts handling.

--- remand/remote/ssh.py
+++ remand/remote/ssh.py
@@ -4,13 +4,18 @@ from paramiko.ssh_exception import SSHException
 from remand import config
 from remand.exc import TransportError
 
+_KNOWN_HOSTS_ERROR = ("The host '{}' was not found in your known_hosts file. "
+"Remand is refusing to connect to unknown hosts.\n\n"
+"If you have an older version of Paramiko installed, this may be because of "
+"a key-type mismatch.\n\n")
+
 
 class SSHRemote(object):
     def __init__(self, hostname, username, port):
         # FIXME: audit this
         self._client = SSHClient()
 
-        if config['ssh'].getboolean('load_system_host_keys'):
+        if config['ssh'].getboolean('load_known_hosts'):
             self._client.load_system_host_keys()
 
         rej_policy = (AutoAddPolicy()
@@ -21,4 +26,6 @@ class SSHRemote(object):
         try:
             self._client.connect(hostname, port or 22, username or 'root')
         except SSHException, e:
+            if 'not found in known_hosts' in e.message:
+                raise TransportError(_KNOWN_HOSTS_ERROR.format(hostname))
             raise TransportError('SSH: {}'.format(e))

f1244f3f81141093e01984ac7e37c7613af5a70f
Marc Brinkmann <git@marcbrinkmann.de>
2015-01-14 02:21:34 +0100

    Removed superflous directory manipulation functions.

--- remand/lib/fs.py
+++ remand/lib/fs.py
@@ -1,38 +1,5 @@
-from functools import reduce
-
 from remand import remote
 from remand.status import changed, unchanged
-
-
-def _path_components(path):
-    """Inverse of path.join.
-
-    >>> _path_components('/a/b/c')
-    ['/', 'a', 'b', 'c']
-    """
-    head = remote.path.normpath(path)
-    acc = []
-
-    while True:
-        head, tail = remote.path.split(head)
-        if not tail:
-            acc.insert(0, head)
-            break
-        acc.insert(0, tail)
-
-    return acc
-
-
-def _subpaths(path):
-    """Returns all subpaths leading up to a path:
-
-    >>> _subpaths('/a/b/c')
-    ['/', '/a', '/a/b', '/a/b/c']
-    """
-    comps = _path_components(path)
-    return reduce(lambda acc, comp: acc + [remote.path.join(acc[-1], comp)],
-                  comps,
-                  [comps.pop(0)])
 
 
 def directory(path, mode=0777):

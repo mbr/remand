655aa09c204145838f478508ead0dd396589f34b
Marc Brinkmann <git@marcbrinkmann.de>
2015-01-21 17:28:15 +0100

    Turn verifiers into classes.

--- remand/lib/fs.py
+++ remand/lib/fs.py
@@ -49,54 +49,107 @@ def _upload_write(local_path, remote_path):
         copyfileobj(src, dst)
 
 
-def _verify_read(st, local_path, remote_path):
-    with remote.file(remote_path, 'rb') as rf,\
-            file(local_path, 'rb') as lf:
+class Verifier(object):
+    registry = {}
 
-        # enable prefetching if files support it
-        # otherwise, performance is horrible (like disabled pipelining)
-        if hasattr(rf, 'prefetch'):
-            rf.prefetch()
+    def verify(self, st, local_path, remote_path):
+        raise NotImplementedError
 
-        bufsize = int(config['buffer_size'])
-        while True:
-            rbuf = rf.read(bufsize)
-            lbuf = lf.read(bufsize)
+    @classmethod
+    def _registered(cls, child):
+        cls.registry[child.short_name] = child
+        return child
 
-            if rbuf != lbuf:
-                return False
+    @classmethod
+    def _by_short_name(cls, short_name):
+        v = cls.registry.get(short_name, None)
+        if v is None:
+            raise ConfigurationError(
+                'Unknown remote file verification method: {!r}. Check your '
+                'fs_remote_*_verify configuration setting.'
+                .format(short_name)
+            )
 
-            # if both files end at the same time, we're good
-            if rbuf == lbuf == '':
-                return True
+        return cls.registry[short_name]
 
-
-def _verify_sha(st, local_path, remote_path):
-    # hash local file
-    with open(local_path, 'rb') as lfile:
-        m = _hash_file(hashlib.sha1, lfile)
-
-        # get remote hash
-        stdout, _ = proc.run([config['cmd_sha1sum'], remote_path])
-        remote_hash = stdout.split(None, 1)[0]
-
-        log.debug('Local hash: {} Remote hash: {}'.format(
-            m.hexdigest(), remote_hash
-        ))
-
-        return remote_hash == m.hexdigest()
+    def __str__(self):
+        return 'Verifier<{}>'.format(self.short_name)
 
 
-def _verify_stat(st, local_path, remote_path):
-    lst = os.stat(local_path)
+@Verifier._registered
+class VerifierIgnore(Verifier):
+    short_name = 'ignore'
 
-    mul = int(config['fs_mtime_multiplier'])
+    def verify(self, st, local_path, remote_path):
+        return False
 
-    # we cast to int, to avoid into issues with different mtime resolutions
-    l = (int(lst.st_mtime * mul), lst.st_size)
-    r = (int(st.st_mtime * mul), st.st_size)
-    log.debug('stat (mtime/size): local {}/{}, remote {}/{}'.format(*(l + r)))
-    return l == r
+
+@Verifier._registered
+class VerifierRead(Verifier):
+    short_name = 'read'
+
+    def verify(self, st, local_path, remote_path):
+        with remote.file(remote_path, 'rb') as rf,\
+                file(local_path, 'rb') as lf:
+
+            # enable prefetching if files support it
+            # otherwise, performance is horrible (like disabled pipelining)
+            if hasattr(rf, 'prefetch'):
+                rf.prefetch()
+
+            bufsize = int(config['buffer_size'])
+            while True:
+                rbuf = rf.read(bufsize)
+                lbuf = lf.read(bufsize)
+
+                if rbuf != lbuf:
+                    return False
+
+                # if both files end at the same time, we're good
+                if rbuf == lbuf == '':
+                    return True
+
+
+@Verifier._registered
+class VerifierSHA(Verifier):
+    short_name = 'sha1sum'
+
+    def verify(self, st, local_path, remote_path):
+        # hash local file
+        with open(local_path, 'rb') as lfile:
+            m = _hash_file(hashlib.sha1, lfile)
+
+            # get remote hash
+            stdout, _ = proc.run([config['cmd_sha1sum'], remote_path])
+            remote_hash = stdout.split(None, 1)[0]
+
+            log.debug('Local hash: {} Remote hash: {}'.format(
+                m.hexdigest(), remote_hash
+            ))
+
+            return remote_hash == m.hexdigest()
+
+
+@Verifier._registered
+class VerifierStat(Verifier):
+    short_name = 'stat'
+
+    def verify(self, st, local_path, remote_path):
+        lst = os.stat(local_path)
+
+        mul = int(config['fs_mtime_multiplier'])
+
+        # we cast to int, to avoid into issues with different mtime resolutions
+        l = (int(lst.st_mtime * mul), lst.st_size)
+        r = (int(st.st_mtime * mul), st.st_size)
+        log.debug('stat (mtime/size): local {}/{}, remote {}/{}'
+                  .format(*(l + r)))
+        return l == r
+
+
+@Verifier._registered
+class VerifierRSync(Verifier):
+    short_name = 'rsync'
 
 
 def upload_file(local_path, remote_path=None):
@@ -142,25 +195,9 @@ def upload_file(local_path, remote_path=None):
             'Not a regular file: {!r}'.format(remote_path)
         )
 
-    verify = config['fs_remote_file_verify']
     upload = config['fs_remote_file_upload']
 
-    if verify == 'stat':
-        verifier = _verify_stat
-    elif verify == 'rsync':
-        raise NotImplementedError('rsync-verify is currently not implemented')
-    elif verify == 'sha1sum':
-        verifier = _verify_sha
-    elif verify == 'read':
-        verifier = _verify_read
-    elif verify == 'ignore':
-        verifier = lambda: False
-    else:
-        raise ConfigurationError(
-            'Unknown remote file verification method: {!r}. Check your '
-            'fs_remote_file_verify configuration setting.'
-            .format(config['fs_remote_file_verify'])
-        )
+    verifier = Verifier._by_short_name(config['fs_remote_file_verify'])()
 
     if upload == 'write':
         uploader = _upload_write
@@ -173,8 +210,8 @@ def upload_file(local_path, remote_path=None):
             .format(config['fs_remote_file_upload'])
         )
 
-    log.debug('verify/upload ({}/{}): {}/{}'.format(
-        verify, upload, verifier, uploader)
+    log.debug('verify/upload: {}/{}'.format(
+        verifier, uploader)
     )
 
     if not os.path.exists(local_path):
@@ -182,7 +219,7 @@ def upload_file(local_path, remote_path=None):
             local_path)
         )
 
-    if not st or not verifier(st, local_path, remote_path):
+    if not st or not verifier.verify(st, local_path, remote_path):
         uploader(local_path, remote_path)
         if config.get_bool('fs_update_mtime'):
             lst = os.stat(local_path)

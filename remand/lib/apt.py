4278463b47a340158da01ba00b4d42a91823db8e
Marc Brinkmann <git@marcbrinkmann.de>
2015-01-22 13:26:08 +0100

    Added last_update and last_upgrade for apt.

--- remand/lib/apt.py
+++ remand/lib/apt.py
@@ -1,9 +1,33 @@
 from collections import OrderedDict
+from datetime import datetime
 
 from debian.deb822 import Deb822
-from remand import log
+from remand import log, remote
 from remand.exc import RemoteFailureError
 from remand.lib import proc
+
+
+def _timestamp_to_datetime(rfn):
+    st = remote.stat(rfn)
+    if not st:
+        ts = 0
+    else:
+        ts = st.st_mtime
+
+    return datetime.fromtimestamp(ts)
+
+
+def info_last_update():
+    # note: may be inaccurate, if the necessary hooks are not set
+    return max(
+        _timestamp_to_datetime('/var/lib/apt/periodic/update-success-stamp'),
+        _timestamp_to_datetime('/var/lib/apt/periodic/update-stamp')
+    )
+
+
+def info_last_upgrade():
+    # note: may be inaccurate, if the necessary hooks are not set
+    return _timestamp_to_datetime('/var/lib/apt/periodic/upgrade-stamp')
 
 
 def query_packages(*pkgs):

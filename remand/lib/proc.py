210c48261c026c81a548aefb61e6c7d4405130f5
Marc Brinkmann <git@marcbrinkmann.de>
2015-03-10 14:59:40 +0100

    WIP commit - not safe for consumption.

--- remand/lib/proc.py
+++ remand/lib/proc.py
@@ -1,6 +1,9 @@
+from binascii import hexlify
 from contextlib import contextmanager
+from os import urandom
 import shlex
 from six.moves import shlex_quote
+from time import time
 
 from remand import remote, log, config
 from remand.exc import RemoteFailureError
@@ -29,26 +32,64 @@ def run(cmd, input=None, extra_env={}):
 
 
 @contextmanager
-def sudo(user=None, password=None):
+def sudo(user=None, password=None, timestamp_timeout=2*60):
     if not isinstance(remote._get_current_object(), SSHRemote):
         raise NotImplementedError('sudo is only supported for SSH remotes.')
 
-    sudo_args = ['sudo', '-E', '-H']
+    sudo_args = ['sudo', '--preserve-env', '--set-home', '--non-interactive']
 
     if user:
-        sudo_args.append('-u')
-        sudo_args.append(user)
-    if password:
-        raise NotImplementedError('Currently, sudo with password is not '
-                                  'supported.')
+        sudo_args.append('--user={}'.format(user))
+
+    prev_timestamp = [0]
 
     def sudo_popen(args, bufsize=0, extra_env={}):
         # -E preserve environment variables passed
         # -H set the $HOME environment variable (usually default)
         # -S (unused): read password from stdin
 
-        sudo_args.extend(args)
-        return orig_popen(sudo_args, bufsize, extra_env)
+        if password:
+            cur = time()
+            if cur - prev_timestamp[0] > timestamp_timeout:
+                # customize our prompt, to prevent accidentally entering the
+                # password. note that this is visible on the sudo invocation
+                # and therefore not a security measure
+                prompt_cookie = hexlify(urandom(40))
+                log.debug('Prompt cookie for sudo refresh: {}'.format(
+                    prompt_cookie)
+                )
+
+                # we need to refresh the sudo timestamp
+                refresh_args = [
+                    'sudo', '--reset-timestamp', '--validate', '--stdin',
+                    '--prompt={}'.format(prompt_cookie)
+                ]
+
+                proc = orig_popen(refresh_args)
+                log.debug('Checking prompt cookie...')
+                if not proc.stderr.read(len(prompt_cookie)) == prompt_cookie:
+                    raise RemoteFailureError('Unexpected output from sudo, '
+                                             'bailing out.')
+
+                # shove in the password
+                stdout, stderr = proc.communicate(password + '\n')
+
+                if proc.returncode != 0:
+                    raise RemoteFailureError(
+                        'Could not refresh sudo timestmap (exit status: {}).'
+                        'The most common occurence for this is an incorrect '
+                        'password.'.format(proc.returncode))
+
+                # from this point on, sudo should work without a password
+                # until the timestamp expires
+                prev_timestamp[0] = cur
+
+                # FIXME: handle SFTP
+
+        pargs = sudo_args[:]
+        pargs.append('--')
+        pargs.extend(args)
+        return orig_popen(pargs + ['--'], bufsize, extra_env)
 
     # monkey patch remote.sudo
     orig_popen = remote.popen
@@ -62,3 +103,5 @@ def sudo(user=None, password=None):
 
     yield
     remote.popen = orig_popen
+
+    # FIXME: remove sudo credentials

e981ff57880f2c4fbe21fd193559e43a44456222
Marc Brinkmann <git@marcbrinkmann.de>
2015-08-18 16:30:17 +0200

    More tests for operations class.

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -1,12 +1,14 @@
 from functools import wraps
 
+import pytest
+
 
 class OperationResult(object):
     changed = None
 
-    def __init__(self, value=None, msg=None):
+    def __init__(self, value=None, message=None):
         self._value = value
-        self.msg = msg
+        self.message = message
 
     @property
     def value(self):
@@ -17,7 +19,7 @@ class OperationResult(object):
 
     def __repr__(self):
         return '{}({!r}, {!r})'.format(self.__class__.__name__, self._value,
-                                       self.msg)
+                                       self.message)
 
 
 class Changed(OperationResult):
@@ -31,9 +33,12 @@ class Unchanged(OperationResult):
 class Failed(OperationResult):
     def __init__(self, exc):
         self.exc = exc
-        self.msg = str(exc)
+        self.message = str(exc)
 
     def value(self):
+        self._reraise()
+
+    def _reraise(self):
         raise self.exc  # from self.exc
 
     def __repr__(self):
@@ -52,6 +57,9 @@ def operation():
                     result = Unchanged(value=rv)
             except Exception as e:
                 result = Failed(e)
+
+            if isinstance(result, Failed):
+                result._reraise()
 
             return result
 
@@ -80,3 +88,110 @@ def test_harmless_operation():
 
     assert isinstance(val, Unchanged)
     assert val.value is None
+    assert val.message is None
+
+
+def test_harmless_operation_with_message():
+    @operation()
+    def does_nothing():
+        return Unchanged(message='test')
+
+    val = does_nothing()
+
+    assert isinstance(val, Unchanged)
+    assert val.value is None
+    assert val.message == 'test'
+
+
+def test_harmless_operation_with_value():
+    @operation()
+    def does_nothing():
+        return 'four'
+
+    val = does_nothing()
+
+    assert isinstance(val, Unchanged)
+    assert val.value is 'four'
+    assert val.message is None
+
+
+def test_harmless_operation_with_value_and_message():
+    @operation()
+    def does_nothing():
+        return Unchanged('four', 'test')
+
+    val = does_nothing()
+
+    assert isinstance(val, Unchanged)
+    assert val.value is 'four'
+    assert val.message == 'test'
+
+
+def test_changing_operation():
+    @operation()
+    def does_nothing():
+        return Changed()
+
+    val = does_nothing()
+
+    assert isinstance(val, Changed)
+    assert val.value is None
+    assert val.message is None
+
+
+def test_changing_operation_with_message():
+    @operation()
+    def does_nothing():
+        return Changed(message='test')
+
+    val = does_nothing()
+
+    assert isinstance(val, Changed)
+    assert val.value is None
+    assert val.message == 'test'
+
+
+def test_changing_operation_with_value():
+    @operation()
+    def does_nothing():
+        return Changed('four')
+
+    val = does_nothing()
+
+    assert isinstance(val, Changed)
+    assert val.value is 'four'
+    assert val.message is None
+
+
+def test_changing_operation_with_value_and_message():
+    @operation()
+    def does_nothing():
+        return Changed('four', 'test')
+
+    val = does_nothing()
+
+    assert isinstance(val, Changed)
+    assert val.value is 'four'
+    assert val.message == 'test'
+
+
+def test_failure_operation_raises():
+    @operation()
+    def oh_oh():
+        raise RuntimeError('damn')
+
+    with pytest.raises(RuntimeError) as exc_info:
+        oh_oh()
+
+    assert exc_info.value.message == 'damn'
+
+
+def test_failure_operation_raises_on_failure_return():
+    @operation()
+    def oh_oh_ret():
+        return Failed(RuntimeError('damn'))
+
+    with pytest.raises(RuntimeError) as exc_info:
+        oh_oh_ret()
+
+    assert exc_info.value.message == 'damn'

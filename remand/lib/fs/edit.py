9cb66812728be4d78fbac7c82567b1646eb905d4
Marc Brinkmann <git@marcbrinkmann.de>
2015-08-24 15:41:30 +0200

    Added line-insertion capabilities to edit file.

--- remand/lib/fs/edit.py
+++ remand/lib/fs/edit.py
@@ -2,6 +2,9 @@ from remand import util, log
 
 
 class EditableFile(object):
+    linesep = '\n'
+    trailing_newline = True
+
     def __init__(self, name):
         self.name = name
         self.initial_hash = self._get_hash()
@@ -12,11 +15,39 @@ class EditableFile(object):
         with self.open('rb') as f:
             return util.hash_file(f)
 
-    def open(self, *args, **kwargs):
-        return open(self.name, *args, **kwargs)
+    def lines(self):
+        with self.open('r') as f:
+            ls = f.read().split(self.linesep)
+
+            # keep the trailing newline
+            if self.trailing_newline and not ls[-1]:
+                ls.pop()
+            return ls
+
+    def set_lines(self, lines):
+        with self.open('w') as f:
+            if self.trailing_newline:
+                lines = lines + ['']
+
+            f.write(self.linesep.join(lines))
+
+    def insert_line(self, line, pos=None, duplicate=False):
+        lines = self.lines()
+        if not duplicate and line in lines:
+            return
+
+        if pos is None:
+            lines.append(line)
+        else:
+            lines.insert(pos, line)
+
+        self.set_lines(lines)
 
     @property
     def modified(self):
         h = self._get_hash()
         log.debug('{} hash currently: {}'.format(self.name, h.hexdigest()))
         return self.initial_hash.digest() != h.digest()
+
+    def open(self, *args, **kwargs):
+        return open(self.name, *args, **kwargs)

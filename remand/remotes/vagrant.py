e0307941e1d937a4d39e21f54b3098dcf5b4f676
Marc Brinkmann <git@marcbrinkmann.de>
2015-10-15 18:12:58 +0200

    More options to create first working vagrant variant.

--- remand/remotes/vagrant.py
+++ remand/remotes/vagrant.py
@@ -5,8 +5,12 @@ from ..uri import Uri
 
 class VagrantRemote(SSHRemote):
     def __init__(self):
-        # just override some settings. first, we connect to localhost
-        config['uri'] = Uri.from_string('ssh://localhost:2222')
-        config['ssh_private_key'] = config['vagrant_private_key']
+        # just override some settings. first, we connect to localhost.
+        # we use 127.0.0.1, because on some machines, "localhost" may resolve
+        # to ::1 first
+        config['uri'] = Uri.from_string('ssh://vagrant@127.0.0.1:2222')
+        config['ssh_private_key'] = config['vagrant_secret_key']
+        config['on_missing_host_key'] = 'ignore'
+        config['use_sudo'] = 'on'
 
         return super(VagrantRemote, self).__init__()

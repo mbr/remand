18082637afc323c0fbc26525fee99e012a1da47c
Marc Brinkmann <git@marcbrinkmann.de>
2016-02-05 01:11:44 +0100

    Yapf setup.py.

--- setup.py
+++ setup.py
@@ -10,38 +10,38 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
-setup(
-    name='remand',
-    version='0.1dev',
-    description='Sane server provisioning.',
-    long_description=read('README.rst'),
-    author='Marc Brinkmann',
-    author_email='git@marcbrinkmann.de',
-    url='http://github.com/mbr/remand',
-    license='MIT',
-    packages=find_packages(exclude=['tests']),
-    install_requires=['appdirs',
-                      'six',
-                      'pluginbase',
-                      'click>=5.0',
-                      'logbook',
-                      'configparser>=3.5.0b2',
-                      'stuf',
-                      'werkzeug',
-                      'volatile>=1.1',
-                      'sshkeys',
-                      'paramiko',  # needs our bugfix!
-                      'sqlalchemy',
-                      'contextlib2',
-                      # chardet is a hidden dependency of python-debian
-                      'python-debian',
-                      'chardet',
-                      'future',
-                      'requests',
-                      'jinja2', ],
-    # FIXME: should guarantee that some packages like
-    # volatile are installed
-    entry_points="""
+setup(name='remand',
+      version='0.1dev',
+      description='Sane server provisioning.',
+      long_description=read('README.rst'),
+      author='Marc Brinkmann',
+      author_email='git@marcbrinkmann.de',
+      url='http://github.com/mbr/remand',
+      license='MIT',
+      packages=find_packages(exclude=['tests']),
+      install_requires=['appdirs',
+                        'six',
+                        'pluginbase',
+                        'click>=5.0',
+                        'logbook',
+                        'configparser>=3.5.0b2',
+                        'stuf',
+                        'werkzeug',
+                        'volatile>=1.1',
+                        'sshkeys',
+                        'paramiko',  # needs our bugfix!
+                        #'sqlalchemy',
+                        #'postgres_pgcatalog',
+                        'contextlib2',
+                        # chardet is a hidden dependency of python-debian
+                        'python-debian',
+                        'chardet',
+                        'future',
+                        'requests',
+                        'jinja2', ],
+      # FIXME: should guarantee that some packages like
+      # volatile are installed
+      entry_points="""
         [console_scripts]
         remand=remand.cli:cli
     """)

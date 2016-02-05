89f243a0826f1d781860ae40012c5427ceb1d4cb
Marc Brinkmann <git@marcbrinkmann.de>
2016-02-05 02:39:31 +0100

    Workaround for older systemd services.

--- remand/lib/systemd.py
+++ remand/lib/systemd.py
@@ -86,7 +86,11 @@ def install_unit_file(unit_file, reload=True):
 @operation()
 def enable_unit(unit_name):
     state = get_unit_state(unit_name)
-    if state['UnitFileState'] == 'enabled':
+
+    # we use 'WantedBy' as a guess whether or not the service is enabled
+    # when UnitFileState is not available (SysV init or older systemd)
+    ufs = state.get('UnitFileState')
+    if ufs == 'enabled' or ufs is None and 'WantedBy' in state:
         return Unchanged(msg='{} already enabled'.format(unit_name))
 
     proc.run([config['cmd_systemctl'], 'enable', unit_name])

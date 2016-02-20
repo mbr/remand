d18532ba2ae8b88de34f3535d6eca04ad3d974e4
Marc Brinkmann <git@marcbrinkmann.de>
2016-02-20 01:37:41 +0100

    Added zip-archive support.

--- remand/lib/archive.py
+++ remand/lib/archive.py
@@ -1,4 +1,5 @@
-from . import proc
+from . import proc, fs
+from remand import remote
 from remand.operation import Changed
 
 TAR_DECOMP_FLAGS = {
@@ -12,18 +13,29 @@ TAR_DECOMP_FLAGS = {
 
 
 def extract(fn, remote_dest):
+    # FIXME: should check if remote_dest is an existing directory or create it
     # FIXME: add a way to extract arbitrary archives locally and send?
-    for ext, flag in TAR_DECOMP_FLAGS.items():
-        if fn.endswith(ext):
-            decomp_flag = flag
-            break
+    if fn.endswith('.zip'):
+
+        with fs.remote_tmpdir() as tmp:
+            archive_name = remote.path.join(tmp, 'archive.zip')
+
+            fs.upload_file(fn, archive_name)
+            proc.run(['unzip', archive_name], cwd=remote_dest)
     else:
-        raise ValueError(
-            'Unsupported archive type (determined by file ending)')
+        # tar
+        for ext, flag in TAR_DECOMP_FLAGS.items():
+            if fn.endswith(ext):
+                decomp_flag = flag
+                break
+        else:
+            raise ValueError(
+                'Unsupported archive type (determined by file ending): {}'
+                .format(fn))
 
-    args = ['tar', decomp_flag + 'xf', '-', '-C', remote_dest]
+        args = ['tar', decomp_flag + 'xf', '-', '-C', remote_dest]
 
-    with open(fn, 'rb') as inp:
-        proc.run(args, input=inp)
+        with open(fn, 'rb') as inp:
+            proc.run(args, input=inp)
 
     return Changed(msg='Extracted archive {} to {}'.format(fn, remote_dest))

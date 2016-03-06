bab9ee8b952c89252ec1b8df59b7bd34b1fd0071
Marc Brinkmann <git@marcbrinkmann.de>
2016-03-06 15:33:07 +0100

    Use init_authorized_keys when setting authorized_keys.

--- remand/lib/ssh.py
+++ remand/lib/ssh.py
@@ -29,37 +29,27 @@ def get_authorized_keys_file(user):
     return ak_file
 
 
+AK_DIR_PERMS = 0o700
+AK_FILE_PERMS = 0o600
+
+
 @operation()
 def set_authorized_keys(files, user='root', fix_permissions=True):
-    DIR_PERMS = 0o700
-    FILE_PERMS = 0o600
+    ak_file = init_authorized_keys(user, fix_permissions)
 
     kf = KeyFile()
 
     for fn in files:
         kf.add_from_file(fn)
 
-    u = info['posix.users'][user]
-    ak_file = config['ssh_authorized_keys_file'].format(name=u.name,
-                                                        home=u.home)
-    log.debug('Authorized key file for {}: {}'.format(u.name, ak_file))
-
-    ak_dir = remote.path.dirname(ak_file)
-
-    dir_creation = fs.create_dir(ak_dir, mode=0o700)
-
-    if dir_creation.changed and fix_permissions:
-        fs.chmod(ak_dir, DIR_PERMS)
-
-    # directory is guaranteed to exist now
-    # FIXME: there's a race condition here, upload_file should support setting
-    #        the file mode
+    # directory is guaranteed to exist now, with correct permissions
     upload = fs.upload_string(str(kf), ak_file)
 
-    if upload.changed or fix_permissions:
-        fs.chmod(ak_file, FILE_PERMS)
+    if upload.changed and fix_permissions:
+        fs.chmod(ak_file, AK_FILE_PERMS)
 
     fps = ', '.join(k.readable_fingerprint for k in kf.keys)
+
     if upload.changed:
         return Changed(
             msg='SSH authorized keys for {} set to: {}'.format(user, fps))
@@ -70,33 +60,32 @@ def set_authorized_keys(files, user='root', fix_permissions=True):
 
 @operation()
 def init_authorized_keys(user='root', fix_permissions=True):
-    DIR_PERMS = 0o700
-    FILE_PERMS = 0o600
-
-    ak_file = _get_authorized_keys_file(user)
+    ak_file = get_authorized_keys_file(user)
     ak_dir = remote.path.dirname(ak_file)
 
     changed = False
 
     # ensure the directory exists
-    changed |= fs.create_dir(ak_dir, mode=DIR_PERMS).changed
+    changed |= fs.create_dir(ak_dir, mode=AK_DIR_PERMS).changed
 
     if fix_permissions:
-        changed |= fs.chmod(ak_dir, DIR_PERMS).changed
+        changed |= fs.chmod(ak_dir, AK_DIR_PERMS).changed
 
     # check if the authorized keys file exists
     if not remote.lstat(ak_file):
         changed |= fs.touch(ak_file).changed
 
     if fix_permissions:
-        changed |= fs.chmod(ak_file, FILE_PERMS).changed
+        changed |= fs.chmod(ak_file, AK_FILE_PERMS).changed
 
     # at this point, we have fixed permissions for file and dir, as well as
     # ensured they exist. however, they might still be owned by root
 
     if changed:
-        return Changed(msg='Changed permissions or owner on authorized keys')
+        return Changed(ak_file,
+                       msg='Changed permissions or owner on authorized keys')
     return Unchanged(
+        ak_file,
         msg='authorized keys file has correct owner and permissions')
 
 

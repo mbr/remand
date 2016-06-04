0b7a2d60cb24b177742c88d4bb9a64c1eff394cb
Marc Brinkmann <git@marcbrinkmann.de>
2016-06-04 21:44:24 +0200

    Added debian systemd timers for unattended upgrades.

---
+++ remand/contrib/debian/__init__.py
@@ -0,0 +1,31 @@
+import os
+
+from remand.lib import systemd
+
+from remand import Plan, operation, Changed, Unchanged
+
+debian = Plan(__name__, os.path.dirname(__file__))
+
+
+@operation()
+def enable_auto_upgrades(boot_time='10min', unit_active_time='1d'):
+    timer_tpl = debian.templates.render('autoupdate.timer',
+                                        boot_time=boot_time,
+                                        unit_active_time=unit_active_time)
+
+    c = False
+    # install both timer and service
+    c |= systemd.install_unit_string('autoupdate.timer', timer_tpl).changed
+    c |= systemd.install_unit_file(debian.files['autoupdate.service']).changed
+
+    # enable both timer and service
+    c |= systemd.enable_unit('autoupdate.timer').changed
+    c |= systemd.enable_unit('autoupdate.service').changed
+
+    # start timer
+    c |= systemd.start_unit('autoupdate.timer').changed
+
+    if c:
+        return Changed(msg='Enabled automatic apt-updates via systemd timer')
+    return Unchanged(
+        msg='Automatic apt-updates via systemd timer already enabled')

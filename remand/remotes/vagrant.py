ff9e0100a1ff0380f5d7933396b7b617796c18f3
Sebastian Bippes <sebastian.bippes@49nord.de>
2017-07-08 22:19:02 +0200

    Automatically remove SSH_AUTH_SOCK environment variable in Vagrant transport.

--- remand/remotes/vagrant.py
+++ remand/remotes/vagrant.py
@@ -21,6 +21,11 @@ class VagrantRemote(SSHRemote):
                 'ensure the file exists and is readable or configure the '
                 '`vagrant_secret_key` setting.'.format(key))
 
+        # most likely we do not want to use the ssh agent when using vagrant,
+        # given that a known private key must be used. to smooth things over,
+        # we unset the SSH_AUTH_SOCK variable here
+        if config.get_bool('vagrant_unset_ssh_auth_sock'):
+            del os.environ['SSH_AUTH_SOCK']
 
         config['ssh_private_key'] = config['vagrant_secret_key']
         config['on_missing_host_key'] = 'ignore'

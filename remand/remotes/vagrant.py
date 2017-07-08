405fb68667f87c5b2925f53f48f5ac8875236cda
Sebastian Bippes <sebastian.bippes@49nord.de>
2017-07-08 22:01:16 +0200

    Added helpful error message in case of missing vagrant secret key.

--- remand/remotes/vagrant.py
+++ remand/remotes/vagrant.py
@@ -1,3 +1,6 @@
+import os
+
+from ..exc import TransportError
 from .ssh import SSHRemote
 from .. import config
 from ..uri import Uri
@@ -9,6 +12,16 @@ class VagrantRemote(SSHRemote):
         # we use 127.0.0.1, because on some machines, "localhost" may resolve
         # to ::1 first
         config['uri'] = Uri.from_string('ssh://vagrant@127.0.0.1:2222')
+
+        key = config['vagrant_secret_key']
+
+        if not os.path.isfile(key):
+            raise TransportError(
+                'Could not find the Vagrant SSH private key at {}. Please '
+                'ensure the file exists and is readable or configure the '
+                '`vagrant_secret_key` setting.'.format(key))
+
+
         config['ssh_private_key'] = config['vagrant_secret_key']
         config['on_missing_host_key'] = 'ignore'
 

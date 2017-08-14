2269b1ecb1930aab56560067c9b8879ef4343fc8
Marc Brinkmann <git@marcbrinkmann.de>
2017-08-14 19:06:39 +0200

    Added nginx module.

---
+++ remand/contrib/nginx/__init__.py
@@ -0,0 +1,27 @@
+import os
+
+from remand.lib import fs, proc, systemd
+
+from remand import Plan, operation, Changed, Unchanged, info
+from remand.util import any_changed
+
+nginx = Plan(__name__, os.path.dirname(__file__))
+
+
+@operation()
+def enable_letsencrypt(auto_reload=True, remove_default=True):
+    changed = any_changed(
+        fs.upload_file(nginx.files['acme-challenge'],
+                       '/etc/nginx/sites-available/acme-challenge'),
+        fs.symlink('/etc/nginx/sites-available/acme-challenge',
+                   '/etc/nginx/sites-enabled/00_acme-challenge'), )
+
+    if remove_default:
+        changed |= fs.remove_file('/etc/nginx/sites-enabled/default').changed
+
+    if changed:
+        if auto_reload:
+            systemd.reload_unit('nginx.service', only_if_running=True)
+
+        return Changed(msg='Enabled nginx Let\'s encrypt support')
+    return Unchanged(msg='nginx Let\'s encrypt support already enabled')
